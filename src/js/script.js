8687dbc1a6e5ec07961e78dc2361dcdc0d77ed0e
marcosmartfern <marcosmartfern@gmail.com>
2026-06-06 15:40:42 +0200

    Añadido javascript para la pagina future

---
+++ src/js/script.js
@@ -0,0 +1,26 @@
+/* Funcion del contenido HTML */
+function changeText() {
+    document.getElementById("futureText").innerHTML =
+        "Artificial Intelligence can generate images, music, videos and interactive content.";
+}
+
+/* Mostrar / ocultar contenido  */
+function toggleContent() {
+
+    var content =
+        document.getElementById("aiContent");
+
+    if (content.style.display == "none") {
+        content.style.display = "block";
+    }
+    else {
+        content.style.display = "none";
+    }
+
+}
+
+/* Validacion de formulario */
+
+/* Integracion de XML */
+
+/* Slider de imágenes */
