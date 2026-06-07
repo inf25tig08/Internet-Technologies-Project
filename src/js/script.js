c23fc45f63efbed9c53fd4cc4bcce43ea8069e3c
marcosmartfern <marcosmartfern@gmail.com>
2026-06-07 14:07:59 +0200

    Añadido animaciones/efectos, show/hide, validacion del formulario, XML cargado con JavaScript

--- src/js/script.js
+++ src/js/script.js
@@ -20,7 +20,95 @@ function toggleContent() {
 }
 
 /* Validacion de formulario */
+function validateForm() {
+
+    var userName =
+        document.getElementById("user-name").value;
+
+    var area =
+        document.getElementById("ai-area").value;
+
+    if (userName == "") {
+
+        alert("Please enter your name.");
+        return false;
+
+    }
+
+    if (area == "") {
+
+        alert("Please choose an AI area.");
+        return false;
+
+    }
+
+    return true;
+
+}
+
 
 /* Integracion de XML */
+function loadXMLData() {
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+            showXMLData(this.responseXML);
+        }
+
+    };
+
+    xhttp.open("GET", "xml/media-evolution.xml", true);
+    xhttp.send();
+
+}
+
+function showXMLData(xml) {
+
+    var technologies = xml.getElementsByTagName("technology");
+
+    var table = "<table>";
+    table += "<thead>";
+    table += "<tr>";
+    table += "<th>Name</th>";
+    table += "<th>Year</th>";
+    table += "<th>Category</th>";
+    table += "<th>Description</th>";
+    table += "</tr>";
+    table += "</thead>";
+
+    table += "<tbody>";
+
+    for (var i = 0; i < technologies.length; i++) {
+
+        table += "<tr>";
+
+        table += "<td>" +
+            technologies[i].getElementsByTagName("name")[0].childNodes[0].nodeValue +
+            "</td>";
+
+        table += "<td>" +
+            technologies[i].getElementsByTagName("year")[0].childNodes[0].nodeValue +
+            "</td>";
+
+        table += "<td>" +
+            technologies[i].getElementsByTagName("category")[0].childNodes[0].nodeValue +
+            "</td>";
+
+        table += "<td>" +
+            technologies[i].getElementsByTagName("description")[0].childNodes[0].nodeValue +
+            "</td>";
+
+        table += "</tr>";
+    }
+
+    table += "</tbody>";
+    table += "</table>";
+
+    document.getElementById("xmlData").innerHTML = table;
+
+}
 
 /* Slider de imágenes */
